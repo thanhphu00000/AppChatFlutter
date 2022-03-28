cdf974f07121ba7106fd5ce1cc6ce04d2b2b4f3b
The Tai Manh <84488032+ttaidev@users.noreply.github.com>
2022-03-29 01:12:09 +0700

    up chat

--- lib/components/chat.dart
+++ lib/components/chat.dart
@@ -26,4 +26,53 @@ class _ChatUsersListState extends State<ChatUsersList> {
           return ChatDetailPage();
         }));
       },
-      
+      child: Container(
+        padding: EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: AssetImage(widget.image),
+                    maxRadius: 30,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(widget.text),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            widget.secondaryText,
+                            style: TextStyle(
+                                fontSize: 14, color: Colors.grey.shade500),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              widget.time,
+              style: TextStyle(
+                  fontSize: 12,
+                  color: widget.isMessageRead
+                      ? Colors.pink
+                      : Colors.grey.shade500),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
