e8a2d42706cadfa12e558dbb803e07a769420431
The Tai Manh <84488032+ttaidev@users.noreply.github.com>
2022-03-31 19:39:59 +0700

    import thu vien

---
+++ lib/modules/chat_page.dart
@@ -0,0 +1,9 @@
+import 'package:chat_app/components/chat.dart';
+import 'package:chat_app/models/chat_users.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
